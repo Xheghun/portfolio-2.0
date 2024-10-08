4c9ec9bac9b0ceaaa48b9ec2fa82f9801b35bd02
Xheghun <ysenruf@gmail.com>
2024-10-08 18:01:30 +0100

    add staff rotation

--- src/app/projects/page.js
+++ src/app/projects/page.js
@@ -1,19 +1,27 @@
 import Image from "next/image";
-import bg from '../../../public/background/projects-background.png'
+import bg from "../../../public/background/projects-background.png";
 import RenderModel from "@/components/RenderModel";
-import Wizard from "@/components/Models/Wizard";
 import ProjectList from "@/components/projects";
 import { projectsData } from "../data";
-
+import Staff  from "@/components/Models/Staff";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between relative">
+      <Image
+        src={bg}
+        alt="background-image"
+        fill
+        className="w-full h-full object-cover object-center opacity-25"
+      />
 
-      <Image src={bg} alt="background-image" fill className="w-full h-full object-cover object-center opacity-25" />
+      <ProjectList projects={projectsData} />
 
-        <ProjectList projects={projectsData} />
-
+      <div className="flex items-center justify-center fixed top-20 left-0 h-screen">
+        <RenderModel>
+          <Staff />
+        </RenderModel>
+      </div>
     </main>
   );
 }
