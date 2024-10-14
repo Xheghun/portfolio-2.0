3ac6c6026fa351415e568abc0e141f92a9fe1c9c
Xheghun <ysenruf@gmail.com>
2024-10-14 20:14:39 +0100

    add sound button

--- src/components/Sound.jsx
+++ src/components/Sound.jsx
@@ -1,10 +1,45 @@
-"use client"
-import React from 'react'
+"use client";
+import React, { useState } from "react";
+import { motion } from "framer-motion";
+import { Volume2, VolumeX } from "lucide-react";
 
 const Sound = () => {
-    return (
-        <div>Sound</div>
-    )
-}
+    
+  const [isPlaying, setIsPlaying] = useState(false);
+  const toggle = () => {
+    setIsPlaying(!isPlaying);
+  };
+
+  return (
+    <div className="fixed top-4 right-2.5 xs:right-4 z-50 group">
+      <audio loop>
+        <source src={"/audio/birds39-forest-20772.mp3"} type="audio/mpeg" />
+        audio element not supported on this browser
+      </audio>
+
+      <motion.button
+        onClick={toggle}
+        initial={{ scale: 0 }}
+        animate={{ scale: 1 }}
+        transition={{ delay: 1 }}
+        className="w-10 h-10 xs:w-14 xs:h-14 text-foreground rounded-full flex items-center justify-center cursor-pointer z-50 p-2.5 xs:p-4 custom-bg"
+        aria-label={"sound"}
+        name={"sound"}
+      >
+        {isPlaying ? (
+          <Volume2
+            className="w-full h-full text-foreground group-hover:text-accent"
+            strokeWidth={1.5}
+          />
+        ) : (
+          <VolumeX
+            className="w-full h-full text-foreground group-hover:text-accent"
+            strokeWidth={1.5}
+          />
+        )}
+      </motion.button>
+    </div>
+  );
+};
 
 export default Sound;
